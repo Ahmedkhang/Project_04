526a48a5f6080f8277034985d21f7089ec392620
Ahmed Ur Rehman <160028006+Ahmedkhang@users.noreply.github.com>
2025-04-13 00:32:35 +0500

    Delete mad_libs directory

--- mad_libs/main.py
+++
@@ -1,6 +0,0 @@
-adj = input('Adjective : ')
-verb1 =input('verb : ')
-verb2 = input('verb : ')
-famous_person = input('famous person : ')
-mad_libs = f"Programming is so {adj}!, It makes me so excited all the time because, I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}"
-print(mad_libs)
