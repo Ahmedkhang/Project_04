d7c25abfd37239a38a6007bbc7122470ee561558
Ahmed Ur Rehman <160028006+Ahmedkhang@users.noreply.github.com>
2025-04-19 20:15:33 +0500

    Update 00_joke_bot.py

--- Assignment_1/00_basics/00_joke_bot.py
+++ Assignment_1/00_basics/00_joke_bot.py
@@ -1,7 +1,12 @@
+# import a random module
 import random
+# get the prompt from user
 prompt : str = 'What do you want? '
+# create a array in which all jokes are stored
 jokes = ['Why do programmers prefer dark mode?Because light attracts bugs', 'Why do Java developers wear glasses?Because they don\'t see sharp', 'How many programmers does it take to change a light bulb?None, that\'s a hardware problem', 'Why do Python programmers prefer using Jupyter notebooks?Because they can\'t handle the pressure of a full IDE', 'Why do programmers hate nature?It has too many bugs']
+# using random module choice method for randomly picking joke
 random_joke = random.choice(jokes)
+
 sorry_statement = 'Sorry, I can only generate joke.'
 
 def main():
